8821bc53a8b1b5de10ce461a261db8a2fb4b71ea
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-03-27 21:01:06 -0600

    Add namespaces examples

---
+++ Namespaces.cs
@@ -0,0 +1,36 @@
+﻿using newdotnet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newdotnet
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Namespaces.SampleMethod();
+            AnotherSampleClass.AnotherSampleMethod();
+        }
+    }
+    class Namespaces
+    {
+        public static void SampleMethod()
+        {
+            Console.WriteLine("Sample method inside Namespaces");
+        }
+    }
+
+    class AnotherSampleClass
+    {
+        public static void AnotherSampleMethod()
+        {
+            Console.WriteLine("SampleMethod inside Namespaces");
+        }
+    }
+}
+
+
