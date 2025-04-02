c3f549469479d1b21738e7df6b9d70a977b32a3c
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-04-01 20:05:39 -0600

    Add inheritance examples

--- ClassAsReference.cs
+++ ClassAsReference.cs
@@ -1,47 +1,47 @@
-﻿using System;
+﻿//using System;
 
-//A type that is defined as a class is a reference type.
-//
-//At run time, when you declare a variable of a reference type, the variable contains the value null until you create an instance of the class by using the *new* operator, or assign it to an object of a compatible type created elsewhere, as shown in the following example.
-
-
-//Declaring an object of type MyClass
-MyClass mc = new MyClass();
-
-//Declaring another object of the same type, assigning it the value of the first object
-
-MyClass mc2 = mc;
-
-mc.MyMethod();
+////A type that is defined as a class is a reference type.
+////
+////At run time, when you declare a variable of a reference type, the variable contains the value null until you create an instance of the class by using the *new* operator, or assign it to an object of a compatible type created elsewhere, as shown in the following example.
 
 
-Customer object2; //This creates an object reference but since is not using the new keyrod, it doesnt create the object itself
+////Declaring an object of type MyClass
+//MyClass mc = new MyClass();
 
-//A reference can refer to an object, either by creating a new object
+////Declaring another object of the same type, assigning it the value of the first object
 
-Customer object3 = new Customer();
+//MyClass mc2 = mc;
 
-//or by assigning it an existing object:
-Customer object4 = object3;
-
-//The code above creates two objects that both refer to the same object. Therefore anychanges made thorugh object3 are reflected in subsequent uses of object4. Because objects that are based on classes are referred to by reference, classess are known as reference types.
+//mc.MyMethod();
 
 
+//Customer object2; //This creates an object reference but since is not using the new keyrod, it doesnt create the object itself
 
-//MyClass.MyMethod(); //this will not compile as it says that 'An object reference is required for the non-static field method, or property 'MyClass.MyMethod', meaning that I need to create an object (instantiate it)'
+////A reference can refer to an object, either by creating a new object
 
-//Creating MyClass (after declaring the object because those lines above count as top level statements)
-class MyClass
-{
-    public void MyMethod()
-    {
-        Console.WriteLine("My text from MyMethod method from MyClass class");
-    }
-}
+//Customer object3 = new Customer();
 
-//[access modifier] - [class] - [identifier]
-public class Customer
-{
-    //Fields, properties, methods and events go here
-}
+////or by assigning it an existing object:
+//Customer object4 = object3;
+
+////The code above creates two objects that both refer to the same object. Therefore anychanges made thorugh object3 are reflected in subsequent uses of object4. Because objects that are based on classes are referred to by reference, classess are known as reference types.
+
+
+
+////MyClass.MyMethod(); //this will not compile as it says that 'An object reference is required for the non-static field method, or property 'MyClass.MyMethod', meaning that I need to create an object (instantiate it)'
+
+////Creating MyClass (after declaring the object because those lines above count as top level statements)
+//class MyClass
+//{
+//    public void MyMethod()
+//    {
+//        Console.WriteLine("My text from MyMethod method from MyClass class");
+//    }
+//}
+
+////[access modifier] - [class] - [identifier]
+//public class Customer
+//{
+//    //Fields, properties, methods and events go here
+//}
 
---
+++ Constructors.cs
@@ -0,0 +1,65 @@
+﻿public class Container
+{
+    //Initialize capacity field to a default value of 10:
+
+    private int _capacity = 10;
+}
+
+public class ContainerTwo
+{
+    private int _capacity;
+
+    public ContainerTwo(int capacity) => _capacity = capacity; //You can require callers to provide an initial value by defining a constructor that's responsible for setting that initial value 
+}
+
+//Beginning C#12  you can define a primary constructor as part of the class declaration:
+public class ContainerThree(int capacity)
+{
+    private int _capcity = capacity;
+}
+
+//Adding parameters to the class name defines the primary constructor. Those parameters are available in the class body, which includes its members. You can use them to initializa fields or anywhere else where they're needed.
+
+//You can also use the required modifier on a property and allow callers to use an object initializer to set the initial value of the property:
+
+public class Person
+{
+    public required string LastName { get; set; }
+    public required string FirstName { get; set; }
+
+
+}
+
+//The addition of the required keyword mandates that callers must set those properties as part of a *new* expression
+
+//var p1 = new Person(); //this will not compile as the required properties are not set
+
+public class Program
+{
+
+
+    public static void Main(string[] args)
+    {
+        PersonObject();
+    }
+
+    public static Person PersonObject()
+    {
+        var p2 = new Person() { FirstName = "Grace", LastName = "Hopper" };
+        Console.WriteLine(p2);
+        return p2;
+    }
+
+}
+
+public class Employee
+{
+
+}
+
+public class Manager : Employee
+{
+    //Employee fields, properties, methods and events are inherited
+
+    //New manager fields, properties methods and events go here...
+}
