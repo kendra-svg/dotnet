c0169e24a3feab4b03e6004601df0ead2741175b
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-05-07 18:38:26 -0600

    Add struct and classes example

--- Constructors.cs
+++ Constructors.cs
@@ -288,29 +288,29 @@ using System.Net.Http.Headers;
 //}
 //Console.ReadLine();
 
-var product = new Product();
-var bonus = new { note = "You won!" };
-var shipment = new { address = "Nowhere St.", product };
-var shipmentWithBonus = new { address = "Somewhere St.", product, bonus };
+//var product = new Product();
+//var bonus = new { note = "You won!" };
+//var shipment = new { address = "Nowhere St.", product };
+//var shipmentWithBonus = new { address = "Somewhere St.", product, bonus };
 
-var anonArray = new[] { new { name = "apple", diam = 4 }, new { name = "grape", diam = 1 }};
+//var anonArray = new[] { new { name = "apple", diam = 4 }, new { name = "grape", diam = 1 }};
 
-var apple = new { Item = "apples", Price = 1.35 };
-var onSale = apple with { Price = 0.79 };
-Console.WriteLine(apple);
-Console.WriteLine(onSale);
+//var apple = new { Item = "apples", Price = 1.35 };
+//var onSale = apple with { Price = 0.79 };
+//Console.WriteLine(apple);
+//Console.WriteLine(onSale);
 
-var v = new { Title = "Hello", Age = 24 };
-Console.WriteLine(v.ToString());
+//var v = new { Title = "Hello", Age = 24 };
+//Console.WriteLine(v.ToString());
 
-class Product
-{
-    public string? Color { get; set; }
-    public decimal Price { get; set; }
-    public string? Name { get; set; }
-    public string? Category { get; set; }
+//class Product
+//{
+//    public string? Color { get; set; }
+//    public decimal Price { get; set; }
+//    public string? Name { get; set; }
+//    public string? Category { get; set; }
 
-    public string? Size { get; set; }
-}
+//    public string? Size { get; set; }
+//}
 
 
--- Types.cs
+++ Types.cs
@@ -23,3 +23,41 @@
 
 //object anotherMessage = "This is a string of characters";
 //IEnumerable<char> someCharacters = "abcdefgfgasjfshdfjksdhf";
+
+using System.Collections.Concurrent;
+
+public class Person()
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age) : this()
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+
+}
+
+class Program()
+{
+    public static void Main()
+    {
+        Person person1 = new Person("name", 0);
+        Console.WriteLine("person1 Name = {0} Age = {1}", person1.Name, person1.Age);
+
+        //Declare new person, assign person1 to it
+        Person person2 = person1;
+
+        //Change the name of person2, and person1 also changes.
+        person2.Name = "Nombre";
+        person2.Age = 22;
+
+        Console.WriteLine("person2 Name = {0} Age = {1}", person2.Name, person2.Age);
+
+        Console.WriteLine("person1 Name = {0} Age = {1}", person1.Name, person2.Age);
+
+
+    }
+}
